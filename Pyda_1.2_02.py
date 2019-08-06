d848ffdca9ee470efd7cca2164f9660b51b23f79
RusRRS <41969531+RusRRS@users.noreply.github.com>
2019-08-06 16:09:34 +0300

    Update Pyda_1.2_02.py

--- Pyda_1.2_02.py
+++ Pyda_1.2_02.py
@@ -4,3 +4,13 @@
 ids = {'user1': [213, 213, 213, 15, 213],
        'user2': [54, 54, 119, 119, 119],
        'user3': [213, 98, 98, 35]}
+
+# Вариант 2. Решение на списках.
+unique_ids_2 = [] # пустой список для уникальных значений
+
+for value_list in ids:
+    ids_list = ids[value_list]
+    for element in ids_list:
+        if element not in unique_ids_2:
+            unique_ids_2.append(element)
+print(unique_ids_2)
