a874b3d0cab8424e5307cc206df2a49a0481011a
RusRRS <41969531+RusRRS@users.noreply.github.com>
2019-08-06 16:10:39 +0300

    Update Pyda_1.2_04.py

--- Pyda_1.2_04.py
+++ Pyda_1.2_04.py
@@ -3,3 +3,13 @@
 # Т. е. в данном примере скрипт должен возвращать 'yandex'.
 
 stats = {'facebook': 55, 'yandex': 120, 'vk': 115, 'google': 99, 'email': 42, 'ok': 98}
+
+# добавим расчет максимальной
+max_sales = 0
+max_sales_channel = ""
+for channel in stats:
+    #print(stats[channel])
+    if  stats[channel] > max_sales:
+        max_sales = stats[channel]     
+        max_sales_channel = channel
+print("Максимальный объем продаж: ", max_sales,"по каналу ", max_sales_channel)
