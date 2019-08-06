98e79a6402ee0927fe907cbf1a8f427ab3dbc277
RusRRS <41969531+RusRRS@users.noreply.github.com>
2019-08-06 16:10:10 +0300

    Update Pyda_1.2_03.py

--- Pyda_1.2_03.py
+++ Pyda_1.2_03.py
@@ -11,3 +11,23 @@ queries = [
     'курс по питону',
     'сериалы про спорт',
 ]
+
+
+element_sum = 0
+target_string = ' '
+output = []
+for element in queries:
+    print(element)
+    print(element.count(' '))
+    output.append(element.count(' ') + 1)
+    
+
+
+
+
+
+print(output)
+print(len(output))
+print("Из трех слов состоят: ", round(output.count(3)/ len(output) * 100, 2)," %", sep="")
+print("Из двух слов состоят: ", round(output.count(2)/ len(output) * 100, 2)," %", sep="")
+print("Из одного слова состоят: ", round(output.count(1)/ len(output) * 100, 2)," %", sep="")
